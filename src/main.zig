870b45f78069a7fbd7c0b83de87e3db3f371cf96
Raúl Wolters <56522208+smups@users.noreply.github.com>
2024-03-06 16:31:13 +0100

    Added functions for parsing all files in a directory

--- src/main.zig
+++ src/main.zig
@@ -8,15 +8,19 @@ const Allocator = std.mem.Allocator;
 /// point to the same file. The `dict` paramter works as follows: passing `.{ .yeet = "skeet" }`
 /// will replace `yeet` with `skeet`. There is no limit to the number of paramters passed in this
 /// way using `dict`. 
-pub fn replace_all(
+pub fn replace_all_unmanaged(
     comptime dict: anytype,
     alloc: Allocator,
     buffer: *std.ArrayList(u8),
     input_file: std.fs.File,
     output_file: std.fs.File
 ) !void {    
+    // Clear buffer
+    buffer.items = [0]u8;
+    
     // Read file
     try input_file.reader().readAllArrayList(buffer, std.math.maxInt(usize));
+    input_file.close();
 
     // Turn buffer into string
     var zig_str = try zstr.fromConstBytes(alloc, buffer.items);
@@ -28,4 +32,41 @@ pub fn replace_all(
 
     //Write shit to file
     try output_file.writer().writeAll(buffer.items);
+    output_file.close();
+}
+
+/// See `replace_all_unmanaged`
+pub fn replace_all(
+    comptime dict: anytype,
+    alloc: Allocator,
+    input_file: std.fs.File,
+    output_file: std.fs.File
+) !void {
+    var buffer = std.ArrayList(u8).init();
+    try replace_all_unmanaged(dict, alloc, &buffer, input_file, output_file);
+}
+
+/// Iterate (recusively) over all elements in `input_dir` and pass them to `replace_all`.
+pub fn replace_all_in_dir(
+    comptime dict: anytype,
+    alloc: Allocator,
+    input_dir: std.fs.Dir,
+    output_dir: std.fs.Dir,
+) !void {
+    // Re-use the same buffer
+    var buffer = std.ArrayList(u8).init();
+    
+    // Iterate over input dir
+    var input_iter = try input_dir.openIterableDir(".", .{});
+    var walker = try input_iter.walk(alloc);
+    while (try walker.next()) |entry| {
+        // Open file for reading
+        const input_file = input_dir.openFile(entry.path, .{});
+
+        // Create output file if it does not exist
+        const output_file = output_dir.createFile(entry.path, .{});
+
+        //Run our replacement procedure
+        try replace_all_unmanaged(dict, alloc, &buffer, input_file, output_file);
+    }
 }
